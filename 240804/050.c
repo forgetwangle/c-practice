44922f933f5ac2dfe9214ba30f238e156caa3e82
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-08-09 19:41:47 +0800

    Update 050.c

--- 240804/050.c
+++ 240804/050.c
@@ -12,3 +12,9 @@ int main(){
     return 0;
 }
 //
+//    int a,n,number=0,i,sum=0;
+//    scanf("%d %d",&a,&n);
+//    for(i=1;i<=n;i++){
+//        number=number*10+a*i;        
+//    }
+//    printf("%d",number);
